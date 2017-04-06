818225b3cbc333f43df1e62c61192ad387a903e5
SoS <1531555719@qq.com>
2017-04-06 09:38:06 +0800

    验证码控制器

---
+++ app/Http/Controllers/YzController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use iscms\Alisms\SendsmsPusher as Sms;
+
+class YzController extends Controller
+{
+    public function __construct(Sms $sms)
+    {
+        $this->sms=$sms;
+    }
+
+    public function index()
+    {
+        $result=$this->sms->send("17600199751","季枫","{'code':'1234'}",'SMS_35975005');
+        echo "11111";
+    }
+}
