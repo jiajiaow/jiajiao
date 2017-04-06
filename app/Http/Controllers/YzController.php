54dd6a2efc3fc2a253243999b7e5b561bffac622
Stone <732691747@qq.com>
2017-04-06 09:58:48 +0800

    y

--- app/Http/Controllers/YzController.php
+++ app/Http/Controllers/YzController.php
@@ -18,6 +18,6 @@ class YzController extends Controller
     public function index()
     {
         $result=$this->sms->send("17600199751","季枫","{'code':'1234'}",'SMS_35975005');
-        echo "1";
+        echo "1111111111";
     }
 }
