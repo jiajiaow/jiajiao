284efd10741b40ed9e8c70e95872267a351097e4
SoS <1531555719@qq.com>
2017-04-06 09:58:07 +0800

    y

--- app/Http/Controllers/YzController.php
+++ app/Http/Controllers/YzController.php
@@ -18,5 +18,6 @@ class YzController extends Controller
     public function index()
     {
         $result=$this->sms->send("17600199751","季枫","{'code':'1234'}",'SMS_35975005');
+        echo "1";
     }
 }
