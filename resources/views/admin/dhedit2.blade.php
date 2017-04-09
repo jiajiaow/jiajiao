d82a8afadc69264e87cd8fef14c771e2a9ee5f30
SoS <1531555719@qq.com>
2017-04-09 11:41:11 +0800

    后台修改模板

---
+++ resources/views/admin/dhedit2.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>添加分站</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="favicon.ico"> <link href="css/bootstrap.min.css?v=3.3.6" rel="stylesheet">
+    <link href="css/font-awesome.css?v=4.4.0" rel="stylesheet">
+    <link href="css/plugins/iCheck/custom.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css?v=4.1.0" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+    <div class="wrapper wrapper-content animated fadeInRight">
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h5>添加分站</h5>
+                        @if(session("msg"))
+                                <h3>{{session("msg")}}</h3>
+                        @endif
+                    </div>
+                    <div class="ibox-content">
+                        <form method="post" action="{{ asset('/admin/dodhedit') }}" class="form-horizontal" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">修改分类:</label>
+                                <div class="col-sm-10">
+                                    @foreach($list as $lis)
+                                    <input type="text" name="erji" placeholder="请输入分类名：" value="{{ $lis->erji }}" class="form-control">
+                                        <input type="hidden" name="id" placeholder="请输入分类名：" value="{{ $lis->id }}" class="form-control">
+                                        <input type="hidden" name="fl"  value="-20" class="form-control">
+                                    @endforeach
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="form-group">
+                                <div class="col-sm-4 col-sm-offset-2">
+                                    <input class="btn btn-primary" type="submit" value="修改"/>
+                                    <button class="btn btn-white" type="submit">取消</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- 全局js -->
+    <script src="js/jquery.min.js?v=2.1.4"></script>
+    <script src="js/bootstrap.min.js?v=3.3.6"></script>
+
+    <!-- 自定义js -->
+    <script src="js/content.js?v=1.0.0"></script>
+
+    <!-- iCheck -->
+    <script src="js/plugins/iCheck/icheck.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.i-checks').iCheck({
+                checkboxClass: 'icheckbox_square-green',
+                radioClass: 'iradio_square-green',
+            });
+        });
+    </script>
+    <script type="text/javascript">
+    //例：北京市
+        $.ajax({
+            url:'/admin/sheng',              //请求地址
+            type:'post',                //请求方式
+            async:true,                 //是否异步
+            success:function(data){     //成功回调函数
+                console.log(data);
+                for (var i = 0; i < data.length; i++) {
+                    $('#cid').append("<option value='"+data[i].provice_id+"'>"+data[i].provice_name+"</option>");
+                };
+            },
+            error:function(){
+                alert('ajax请求失败');  //失败回调
+            }
+        });
+
+    //例：北京市 - 市辖区（市辖县）
+
+        $("#cid").change(function(){
+            $(this).nextAll("select").remove();
+            var ob = $(this);
+            //alert(ob.val());
+            $.ajax({
+                url:'/admin/xian',              //请求地址
+                type:'post',                //请求方式
+                async:true,                 //是否异步
+                data:{pid:($(this).val())}, //发送的数据
+                dataType:'json',            //响应的数据类型
+                success:function(data){     //成功回调函数
+                    console.log(data);
+                    if(data.length>0){
+                        var select = $("<select id='city' class='form-control m-b' name='city'><option>--请选择--</option></select>")
+                        for (var i = 0; i < data.length; i++) {
+                            $(select).append("<option value='"+data[i].city_id+"'>"+data[i].city_name+"</option>");
+                        };
+                        ob.after(select);
+                    }
+                },
+                error:function(){
+                    alert('ajax请求失败');  //失败回调
+                }
+            });
+        });
+    </script>
+
+
+
+</body>
+
+</html>
