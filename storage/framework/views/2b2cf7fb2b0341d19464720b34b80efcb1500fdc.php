2d448764bb02ce3ca5ddbe25f10074e4d6e9eb82
Stone <732691747@qq.com>
2017-05-25 17:06:18 +0800

    env

--- storage/framework/views/2b2cf7fb2b0341d19464720b34b80efcb1500fdc.php
+++ storage/framework/views/2b2cf7fb2b0341d19464720b34b80efcb1500fdc.php
@@ -17,8 +17,6 @@
      <script type="text/javascript">
         if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
             window.location.href="/mobile";
-        }else{
-            window.location.href="/";
         }
       </script>
      <style>
